f3a89dd5881e713451cf3d600b20e4f1b31b4d34
Janssen <noconsulate@gmail.com>
2019-09-03 17:53:17 -0600

    uibuild

---
+++ build/precache-manifest.92a2b7eb6d2298048e03cc285dc10280.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d7c4492622672a03f111d54a475f7e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "997bafc2fd5fb0a48772",
+    "url": "/static/js/2.18074af6.chunk.js"
+  },
+  {
+    "revision": "972f848dcd93de405890",
+    "url": "/static/js/main.9e995f35.chunk.js"
+  },
+  {
+    "revision": "643b1496a8e20922303c",
+    "url": "/static/js/runtime~main.bf59cc56.js"
+  }
+]);
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 
 app.use(cors())
 app.use(bodyParser.json())
-
+app.use(express.static('build'))
 
 const logger = morgan(':method :url :status :res[content-length] - :response-time ms :reqObj')
 
